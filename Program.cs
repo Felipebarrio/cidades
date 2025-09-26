3163742c9b9fb263398378177b96af2ad6aebc44
felipe barrio <felipebariof@gmail.com>
2025-09-26 19:24:30 -0300

    realizando atividade

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Peruibe");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Ilha comprida");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Iguape");
+Console.WriteLine("Eldorado");
+Console.WriteLine("Mongaguá");
